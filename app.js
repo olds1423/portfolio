bcb1ea8eb795a9a9c2ff8f500158027b41fe5868
olds1423 <olds1423@gmail.com>
2016-12-04 21:03:36 -0800

    sudo coded out the rest of the porfolio assignment 2

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-Project.allProjects = [];
+// Project.allProjects = [];
+//
+// Project.prototyope.toHtml = function(templateId) {
+//   var template = Handlebars.compile($(templateId).text());
+//   return template(this);
+// };
 
-Project.prototyope.toHtml = function(templateId) {
-  var template = Handlebars.compile($(templateId).text());
-  return template(this);
-};
+//complete a constructor function that creates objects from all of my data file.
